ee3bb269962529c5b90f0f84af80a0be9da81dc9
Micaela <mika.dure1105@gmail.com>
2023-11-28 23:55:26 -0300

    first commit

---
+++ models/rol.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Rol extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Rol.hasOne(models.Personal, {
+        foreignKey: 'rolId',
+        as: 'personal'
+      })
+    }
+  }
+  Rol.init({
+    cargo: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Rol',
+  });
+  return Rol;
+};
